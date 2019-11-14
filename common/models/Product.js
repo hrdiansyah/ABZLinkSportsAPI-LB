442ad27e6ebfdcfd88f07dacf81f6a38b95a35e5
ahmad-TG <ahmadroyani913@gmail.com>
2019-11-14 18:12:34 +0700

    revisi

--- common/models/Product.js
+++ common/models/Product.js
@@ -35,4 +35,38 @@ module.exports = function(Product) {
         }
     );
 
+    Product.getProductByName = function(name,  callback) {
+        new Promise(function(resolve, reject) {
+         //find name
+                    Product.find( { where :{product_name : { like : name } } }, function(err, result){
+                        if (err) reject (err);
+                        if (result === null){
+                            err = new Error("User Not Found");
+                            err.statusCode =  404;
+                            reject (err)
+                        }
+                        resolve(result);
+                    });
+                }).then(function(res) {
+                    if (!res) callback (err);
+                    return callback(null, res);
+                }).catch(function (err) {
+                    callback (err);
+                });
+            };
+    
+        Product.remoteMethod(
+            'getProductByName',
+            {
+                description : 'get user by name',
+                accepts: [
+                    {arg : 'product_Name', type : 'string'}
+                ],
+                returns : {
+                    arg : 'res', type: 'object', root: true
+                },
+                http:{path: '/getProductByName', verb: 'get'},
+            }
+        );
+
 };
