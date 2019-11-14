97e6be36eebe3cefdb6c53f3f794f72af47e4f5f
hrdiansyah <Hardiancrown@gmail.com>
2019-11-14 09:29:31 +0700

    container diskon

---
+++ common/models/containerdsc.js
@@ -0,0 +1,34 @@
+module.exports = function(containerdsc) {
+
+    containerdsc.getContainerdscByName= function(CONTAINERDSC, callback){
+        new Promise (function(resolve, reject){
+            //find by name
+        containerdsc.find({where:{Name:{like: CONTAINERDSC}}}, function(err, result){
+            if(err) reject(err);
+            if (result== null){
+                err = new Error('Maaf tidak ditemukan');
+                err.statusCode=404;
+                reject(err);
+            }
+            resolve(result);
+        });
+        }).then(function(res){
+            if (!res) callback(err);
+            return callback(null, res[0]);     
+        }).catch(function(err){
+            callback(err);
+        });
+    };
+    containerdsc.remoteMethod(
+        'getContainerdsc',{
+        description : 'get discount by name',
+        accepts: [
+            {arg : 'name', type : 'string'}
+        ],
+        returns : {
+            arg : 'res', type: 'object', root: true
+        },
+        http:{path: '/getContainerdscByName', verb: 'get'},
+    }
+);
+};
